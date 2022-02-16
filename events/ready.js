cdf75db1243f1e1e1324823182f0a39be5fd3d37
Fynnyx <westerathf@gmail.com>
2022-02-16 16:40:32 +0100

    update status

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
 const client = require("../index.js")
+const data = require(`${process.cwd()}/properties.json`)
 
 
 client.on("ready", () => {
     console.info(`\x1b[33m${client.user.username}\x1b[34m, logged in\x1b[0m`)
-    client.user.setActivity(`TestBot Startet`)
+    client.user.setActivity({ type: "WATCHING", name: data.events.status.message })
 })
