31953219f33c5bdd8acea8e9a5a1422bfd30c56c
Fynnyx <westerathf@gmail.com>
2022-02-16 16:40:56 +0100

    made agents page system

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,13 @@
 const client = require('../index.js')
+const { getAgentEmbed } = require("../helpers/getEmbed");
+const { getAgentsRow } = require("../helpers/getActionRow");
+const { sleep } = require('../helpers/sleep.js');
 
 
 client.on('interactionCreate', async interaction => {
 	// DONT CHANGE THIS CODE
 	// It calls the right SlashCommand run function.
-	if (interaction.isCommand) {
-		// await interaction.deferReply({ ephemeral: false }).catch(() => { });
+	if (interaction.isCommand()) {
 
 		const cmd = client.slashCommands.get(interaction.commandName);
 		if (!cmd)
@@ -26,6 +28,29 @@ client.on('interactionCreate', async interaction => {
 		cmd.run(client, interaction, args);
 	}
 
+	if (interaction.isButton()) {
+		console.log("Button pressed");
+		switch (interaction.customId.toLowerCase()) {
+			case "agentsdown":
+				let agentsDownPage = parseInt(interaction.message.embeds[0].footer.text.split("/", 1)[0])
+				interaction.message.edit({ embeds: [await getAgentEmbed(agentsDownPage - 1)], components: [await getAgentsRow(agentsDownPage - 1)] })
+				var agentsDownMsg
+				if (!interaction.replied) {
+					agentsDownMsg = await interaction.reply({ content: "Page Switched", fetchReply: true })
+				}
+				if (!interaction === undefined) {
+					agentsDownMsg.delete()
+				}
 
+			case "agentsup":
+				let agentsUpPage = parseInt(interaction.message.embeds[0].footer.text.split("/", 1)[0])
+				interaction.message.edit({ embeds: [await getAgentEmbed(agentsUpPage + 1)], components: [await getAgentsRow(agentsUpPage + 1)] })
+				var agentsUpMsg
+				if (!interaction.replied) {
+					agentsUpMsg = await interaction.reply({ content: "Page Switched", fetchReply: true })
+				}
+				agentsUpMsg.delete()
+		}
+	}
 	// --> Here you can go on.
 });
